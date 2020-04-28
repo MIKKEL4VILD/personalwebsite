15fc203f7050c31d0d0da69b2a1e61353b7464b8
Mikkel Wessel Nielsen <mwn@Mikkels-MacBook-Pro.local>
2020-04-28 11:01:19 +0200

    new design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+        <div className="NavContent">
+          <h1>Title</h1>
+          <ul>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+            <li>Menu Item</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="Content">
+        <div className="SideBar">
+          <div className="List">
+            <h1>List</h1>
+            <ul>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Item</li>
+            </ul>
+          </div>
+          <div className="Tags">
+            <h1>Tags</h1>
+            <ul>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Big Item</li>
+              <li>Item</li>
+              <li>Item</li>
+              <li>Big Item</li>
+            </ul>
+          </div>
+        </div>
+        <div className="MainContent">
+          <div className="TextItem">
+            <h1>
+              Headline
+          </h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry’s
+              standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged … <a href="/">Read more</a>
+          </p>
+          </div>
+          <hr />
+          <div className="TextItem">
+            <h1>
+              Headline
+          </h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry’s
+              standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting,
+              remaining essentially unchanged. It was popularised in the
+              1960s with the release of Letraset sheets containing
+              Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+          </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
